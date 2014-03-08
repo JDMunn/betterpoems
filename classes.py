f5e2d6c354839de872390f8dfd29c6d57571ccd7
Ted Kornish <ted.kornish+github@gmail.com>
2014-03-08 12:15:11 -0700

    Add poems#view_annotations() method

--- classes.py
+++ classes.py
@@ -5,10 +5,16 @@ class Poem:
     self.text_arr = self.text.split()
     self.annotations = []
 
-  def add_annotation(start, end, content, print_output = True):
+  def add_annotation(self, start, end, content, print_output = True):
     self.annotations.append(Annotation(start, end, content))
     if print_output:
       print "Added annotation!"
+
+  def view_annotations(self):
+    for a in self.annotations:
+      print 'text:', " ".join(self.text_arr[a.start:a.end])
+      print 'annotation:', a.content
+      print
 
 class Annotation:
   def __init__(self, start, end, content):
