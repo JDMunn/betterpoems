debd785a3af290f7ad8d28f077745422c223bbd5
Ted Kornish <ted.kornish+github@gmail.com>
2014-03-08 12:21:11 -0700

    Start redoing annotations

--- classes.py
+++ classes.py
@@ -2,23 +2,28 @@ class Poem:
   def __init__(self, filename):
     with open(filename) as f:
       self.text = f.read()
-    self.text_arr = self.text.split()
+    self.words = self.text.split()
+    self.lines = self.text.split("\n")
     self.annotations = []
 
-  def add_annotation(self, start, end, content, print_output = True):
+  def annotate_word(self, start, end, content, print_output = True):
     self.annotations.append(Annotation(start, end, content))
     if print_output:
       print "Added annotation!"
 
+  def annotate_line(self, start, end, content, print_output = True):
+
+
   def view_annotations(self):
     for a in self.annotations:
-      print 'text:', " ".join(self.text_arr[a.start:a.end])
+      print 'text:', " ".join(self.words[a.start:a.end])
       print 'annotation:', a.content
       print
 
 class Annotation:
-  def __init__(self, start, end, content):
+  def __init__(self, start, end, content, type):
     # Takes a poem object as argument
     self.start = start
     self.end = end
     self.content = content
+    self.type = type # 'line' or 'word'
